8aa769dd23cb2933d0d77b4ab15685694be5d802
Rafal Ciszewski <rafal.cisz1991@gmail.com>
2021-11-13 18:48:30 +0100

    New Commit

--- src/main/java/com/example/mycalculator/MyCalculatorApplication.java
+++ src/main/java/com/example/mycalculator/MyCalculatorApplication.java
@@ -6,8 +6,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyCalculatorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MyCalculatorApplication.class, args);
+    public void addingNumbers(int numberA, int numberB){
+
+        int addingResult = numberA + numberB;
+        System.out.println("Result of adding " + numberA + " and " + numberB + " is " + addingResult);
     }
+
+    public void subtractingNumbers(int numberA, int numberB){
+
+        int subtractionResult = numberA - numberB;
+        System.out.println("Result of subtracting " + numberA + " and " + numberB + " is " + subtractionResult);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Calculator works");
+
+        MyCalculatorApplication calculator = new MyCalculatorApplication();
+        calculator.addingNumbers(7, 5);
+        calculator.subtractingNumbers(10,8);
+
+    }
+
 
 }
